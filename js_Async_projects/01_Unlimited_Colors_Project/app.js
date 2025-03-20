8b7c3794a89160ae7415c34670c5af1adf487f96
ahmedcurious <ahmed_naseem1@outlook.com>
2025-03-21 00:17:31 +0500

    js_async_project_01

---
+++ js_Async_projects/01_Unlimited_Colors_Project/app.js
@@ -0,0 +1,54 @@
+const body = document.querySelector("body");
+const startButton = document.querySelector("#start");
+const stopButton = document.querySelector("#stop");
+const colorCodeDisplay = document.querySelector("#color-code");
+let color;
+
+//This function generates color in RGB values
+function changeColor() {
+  color = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(
+    Math.random() * 255
+  )}, ${Math.round(Math.random() * 255)})`;
+  startButton.disabled = true;
+  body.style.backgroundColor = color;
+  colorCodeDisplay.textContent = color;
+}
+
+//This function generates color in HEXCODE
+function randomColorHex() {
+  const hex = "0123456789ABCDEF";
+  color = "#";
+  for (let i = 0; i < 6; i++) {
+    let randomNum_16 = Math.floor(Math.random() * 16);
+    color += hex[randomNum_16];
+  }
+  startButton.disabled = true;
+  body.style.backgroundColor = color;
+  colorCodeDisplay.textContent = color;
+}
+// randomColorHex();
+
+let colorInterval;
+
+startButton.addEventListener(
+  "click",
+  (e) => {
+    console.log("Color Changing Started");
+    if (!colorInterval) {
+      colorInterval = setInterval(randomColorHex, 1000);
+    }
+  },
+  false
+);
+
+stopButton.addEventListener(
+  "click",
+  (e) => {
+    console.log("Color Changing Stopped");
+    clearInterval(colorInterval);
+    colorInterval = null;
+    console.log(`Color RGB Value: ${color}`);
+    startButton.disabled = false;
+  },
+  false
+);
